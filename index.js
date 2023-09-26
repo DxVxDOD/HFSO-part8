c1c9b9e8130f9b53350aa5335f13a5b026b0b5be
DxVxDOD <orbangdavid@yahoo.com>
2023-09-26 23:17:28 +0300

    8.3: All authors

--- index.js
+++ index.js
@@ -106,10 +106,18 @@ const typeDefs = `
         genres: [String!]!
   }
   
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+  
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Books!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -117,7 +125,15 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books
+        allBooks: () => books,
+        allAuthors: () => authors
+    },
+
+    Author: {
+        bookCount: (root) => {
+        const bookCount = books.filter( book => book.author === root.name )
+            return bookCount.length
+        }
     }
 }
 
