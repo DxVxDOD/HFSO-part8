a8622b58751be0d02e2c905137cca75cf9464d87
DxVxDOD <orbangdavid@yahoo.com>
2023-09-26 19:07:39 +0300

    8.2: All books

--- index.js
+++ index.js
@@ -98,16 +98,26 @@ let books = [
 */
 
 const typeDefs = `
+  type Books {
+        title: String!
+        published: Int!
+        author: String!
+        id: ID!
+        genres: [String!]!
+  }
+  
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books
     }
 }
 
